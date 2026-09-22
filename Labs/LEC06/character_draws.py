5a1f54d6c7ec3eb470701a62828c6f274c28937a
YJS <YJS@users.noreply.github.com>
2026-09-22 23:21:02 +0900

    캐릭터 그리기 스크립트의 캔버스 설정 및 정리 리팩터링

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,9 @@
 # 실습 과제 진행
+
+from pico2d import*
+
+open_canvas(800,600)
+
 def move_circle():
     print("Circle")
     pass
@@ -17,3 +22,5 @@ while True:
     move_rectangle()
     move_triangle()
     pass
+
+close_canvas()
